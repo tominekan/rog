c05a5dd9fe1c37b8477d3346d64e160398c17351
Tomi Adenekan <tominekan@outlook.com>
2021-06-06 16:43:06 -0700

    Added parsing for alphabet, added beginning code for choosing the correct type for the lines

--- rog.c
+++ rog.c
@@ -4,15 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 // Error definitions
-#define SYNTAX_ERROR -1
-
-// Type definitions, there are literally only three types of strings
-// Integers, letters of the alphabet, and special characters (symbol)
-#define NUM_T
-#define ALPHABET_T
-#define SYMBOL_T
+#define ROG_ERROR -1
 
 // These are a list of all the special characters
 const char SPECIAL_CHARS[35] = {
@@ -53,27 +48,59 @@ const char SPECIAL_CHARS[35] = {
     '\0'
 };
 
+const char ALPHABET[27] = {
+    'a',
+    'b',
+    'c',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    '\0'
+};
+
 u_int8_t process_num(char *line) {
     size_t length = strlen(line);
     if (length > 10) {
-        return SYNTAX_ERROR;
+        return ROG_ERROR;
     } else {
-        return length - 1;
+        return length - 1; // for zero based indexing
     }
 }
 
-char process_char(char *line) {
-    size_t length = strlen(line);
-
-    if (length > 1) {
-        void;
+char process_char(char *line, bool is_upper) {
+    size_t length = strlen(line) - 1; // for null terminator
+    if (length > 26) { // There cannot be more than 26 letters in the english alphabet
+        return ROG_ERROR;
+    } else {
+        return ALPHABET[length-1]; // accounting for zero based indexing
     }
 }
+
 int main() {
     FILE *file_pointer;
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
+    bool is_upper = false;
 
     file_pointer = fopen("hello_world.rg", "r");
 
@@ -86,6 +113,11 @@ int main() {
     // Read the file line by line and print it out
     while ((read = getline(&line, &len, file_pointer) != EOF)) {
         printf("%s", line);
+
+        if (strchr(line, '+') != NULL) { // If it defines a string
+            // TODO: Implement parsing for both upper and lower case symbols
+            printf("Here I need to implement some of the above");
+        }
     }
 
     fclose(file_pointer);
