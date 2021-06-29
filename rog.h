15e4c0338a73e6ebeb4d0e1c8d731be8e8fae14a
Tomi Adenekan <tominekan@outlook.com>
2021-06-29 17:38:13 +0000

    Updated files to match function calls in rog.c

--- rog.h
+++ rog.h
@@ -3,6 +3,9 @@
 // Check out the corresponding rog.c file for explanations
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 #define ROG_ERROR -1
 
@@ -14,4 +17,13 @@ int8_t validate_char(char *line, char *file_name, int line_no, bool is_upper);
 
 char process_symbol(char *line);
 int8_t validate_symbol(char *line, char *file_name, int line_no);
+
+
+void error_print(char *file_name, char *line_content, char *error_message, int line_no);
+
+void execute_file(char *file_name);
+
+void print_help();
+
+void process_argument();
 #endif
