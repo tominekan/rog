2eac9e52142d42ed5314a3dc91b2ee7cba6fb6a5
Tomi Adenekan <tominekan@outlook.com>
2021-07-06 15:50:53 +0000

    Added checks.sh

--- checks.sh
+++ checks.sh
@@ -11,17 +11,38 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 RESET='\033[0;0m'
 requirements_are="met"
-GCC_EXISTS_CHECK=$(return_code "gcc --version")
 echo "This script checks if you have tools required to run rog"
 echo "These tools are needed:"
-echo "    - gcc >= 8.3.0"
-echo "    - make >= 4.3.1"
-echo "    - GNU coreutils >= 8.30 (should already be installed)"
+echo "    - gcc (v8.3.0 or greater preferred)"
+echo "    - make (v4.2.1 or greater preferred)"
+echo "    - GNU coreutils, specifically install (should already be installed)"
 
 echo "You ..."
-if [ $GCC_EXISTS_CHECK -ne 0 ]
+# Checks if gcc is installed (by seeing if gcc --version works)
+$(which gcc)  --version > /dev/null 2> /dev/null
+if [ $? -ne 0 ]
 then
     echo "${RED}    - Don't have gcc ${RESET}"
 else
     echo "${GREEN}    - Have gcc ${RESET}"
+fi
+
+# Checks if make is installed (by seeing if make --version works)
+$(which make) --version > /dev/null 2> /dev/null
+if [ $? -ne 0 ]
+then
+    echo "${RED}    - Don't have make ${RESET}"
+else
+    echo "${GREEN}    - Have make ${RESET}"
+fi
+
+# Checks if GNU coreutils is installed by checking install,
+# the coreutils tool we will be using, since install is 
+# a part of the GNU coreutils
+$(which install) --version > /dev/null 2> /dev/null
+if [ $? -ne 0 ]
+then
+    echo "${RED}    - Don't have install ${RESET}"
+else
+    echo "${GREEN}    - Have install ${RESET}"
 fi
